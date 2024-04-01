75d0aaa50bf37fad726d9aeed19333fbd8e77447
OnArap <onarap95@gmail.com>
2024-04-01 19:38:07 +0200

    WebView for Twitch player, need to fixe

--- lib/core/widgets/twitch_player.dart
+++ lib/core/widgets/twitch_player.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class TwitchPlayer extends StatefulWidget {
-  final String video = "https://www.twitch.tv/rocketbaguette";
-
   const TwitchPlayer({super.key});
 
   @override
@@ -10,20 +9,37 @@ class TwitchPlayer extends StatefulWidget {
 }
 
 class _TwitchPlayerState extends State<TwitchPlayer> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://player.twitch.tv/?channel=rocketbaguette&enableExtensions=true&muted=false&parent=twitch.tv&quality=chunked&volume=0.50'));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.30,
-        width: MediaQuery.of(context).size.width - 25,
-        decoration: BoxDecoration(color: Theme.of(context).colorScheme.secondaryContainer, borderRadius: const BorderRadius.all(Radius.circular(5))),
-        child: Center(
-          child: Text(
-            "Player",
-            style: TextStyle(color: Theme.of(context).colorScheme.primary),
-          ),
-        ),
-      ),
-    );
+        child: Container(
+      height: MediaQuery.of(context).size.height * 0.30,
+      width: MediaQuery.of(context).size.width - 25,
+      decoration: BoxDecoration(color: Theme.of(context).colorScheme.secondaryContainer, borderRadius: const BorderRadius.all(Radius.circular(5))),
+      child: WebViewWidget(controller: _controller),
+    ));
   }
 }
